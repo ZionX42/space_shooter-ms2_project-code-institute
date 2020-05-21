301b6d4bf706ebfd1e50fe429198ccb129f851fb
Rick N <rickmnassar@gmail.com>
2020-05-21 15:40:50 +0100

    Comma & simi colum errors in man.js

--- js/main.js
+++ js/main.js
@@ -1,66 +1,67 @@
 // Resetting player function
 function resetPlayer() {
-  if (GameManager.player == undefined) {
-    let asset = GameManager.assets["rick_spacecraft.png"];
+    if (GameManager.player == undefined) {
+        let asset = GameManager.assets["rick_spacecraft.png"];
 
-    GameManager.player = new Player(
-      GameSettings.playerDivName,
-      new Point(GameSettings.playerStart.x, GameSettings.playerStart.y),
-      asset.fileName,
-      new Size(asset.width, asset.height)
-    );
-    GameManager.player.addToBoard(true);
-  }
-  console.log("resetplayer() GameManager.player:", GameManager.player);
-  GameManager.player.reset();
+        GameManager.player = new Player(
+            GameSettings.playerDivName,
+            new Point(GameSettings.playerStart.x, GameSettings.playerStart.y),
+            asset
+        );
+        GameManager.player.addToBoard(true);
+    }
+    console.log("resetplayer() GameManager.player:" , GameManager.player);
+    GameManager.player.reset();
 }
 
 // Image loading
 function init() {
-  console.log("Main Game init()");
-  resetPlayer();
+    console.log("Main Game init()");
+    resetPlayer();
+
 }
 
 function processAsset(indexNum) {
-  let img = new Image();
-  let fileName = "assets/" + ImageFiles[indexNum] + ".png";
-  img.src = fileName;
-  img.onload = function () {
-    GameManager.assets[ImageFiles[indexNum]] = {
-      width: this.width,
-      height: this.height,
-      fileName: fileName,
-    };
-    indexNum++;
-    if (indexNum < ImageFiles.length) {
-      processAsset(indexNum);
-    } else {
-      console.log("Assets Done:", GameManager.assets);
-      init();
+    let img = new Image();
+    let fileName = "assets/" + ImageFiles[indexNum] + ".png";
+    img.src = fileName;
+    img.onload = function () {
+        GameManager.assets[ImageFiles[indexNum]] = {
+            width: this.width,
+            height: this.height,
+            fileName: fileName
+        };
+        indexNum++;
+        if (indexNum < ImageFiles.length) {
+            processAsset(indexNum);
+        } else {
+            console.log("Assets Done:", GameManager.assets);
+            init();
+        }
     }
-  };
 }
 
 // Testing which key controls can be used
 $(function () {
-  processAsset(0);
-  $(document).keydown(function (e) {
-    switch (e.which) {
-      case GameSettings.keyPress.up:
-        console.log("up");
-        break;
-      case GameSettings.keyPress.down:
-        console.log("down");
-        break;
-      case GameSettings.keyPress.left:
-        console.log("left");
-        break;
-      case GameSettings.keyPress.right:
-        console.log("right");
-        break;
-      case GameSettings.keyPress.space:
-        console.log("space");
-        break;
-    }
-  });
+    processAsset(0);
+    $(document).keydown(function (e) {
+            switch (e.which) {
+                case GameSettings.keyPress.up:
+                    console.log("up");
+                    break;
+                case GameSettings.keyPress.down:
+                    console.log("down");
+                    break;
+                case GameSettings.keyPress.left:
+                    console.log("left");
+                    break;
+                case GameSettings.keyPress.right:
+                    console.log("right");
+                    break;
+                case GameSettings.keyPress.space:
+                    console.log("space");
+                    break;
+            }
+        }
+    );
 });
