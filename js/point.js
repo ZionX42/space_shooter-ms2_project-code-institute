eaab3d9710404b0824e6e8883d23f8845d017c35
Rick N <rickmnassar@gmail.com>
2020-05-21 14:49:48 +0100

    Added new src to index.html

---
+++ js/point.js
@@ -0,0 +1,5 @@
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
---
+++ js/size.js
@@ -0,0 +1,11 @@
+// Declaring size of the objects
+
+class Size {
+    constructor(w,h) {
+        this.width = w;
+        this.height = h;
+    }
+}
+
+let s = new Size (19 , 30);
+console.log ("s:" , s);
